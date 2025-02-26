f99ebcc1090083fef4bfd3da60d96be5341df01b
ALfredo <alfredobch2004@gmail.com>
2025-02-26 15:02:46 -0600

    Maquetado completo

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -1,14 +1,13 @@
 import { Container, Row, Col, Tab, Nav } from "react-bootstrap";
-import { ProjectCard } from '../components/ProjectCard.jsx';
+import { ProjectCard } from "../components/ProjectCard.jsx";
 import projImg1 from "../assets/img/project-img1.png";
 import projImg2 from "../assets/img/project-img2.png";
 import projImg3 from "../assets/img/project-img3.png";
 import colorSharp2 from "../assets/img/color-sharp2.png";
-import 'animate.css';
-import TrackVisibility from 'react-on-screen';
+import "animate.css";
+import TrackVisibility from "react-on-screen";
 
 export const Project = () => {
-
   const projects = [
     {
       title: "Business Startup",
@@ -48,51 +47,112 @@ export const Project = () => {
         <Row>
           <Col size={12}>
             <TrackVisibility>
-              {({ isVisible }) =>
-              <div className={isVisible ? "animate__animated animate__fadeIn": ""}>
-                <h2>Projects</h2>
-                <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
-                <Tab.Container id="projects-tabs" defaultActiveKey="first">
-                  <Nav variant="pills" className="nav-pills mb-5 justify-content-center align-items-center" id="pills-tab">
-                    <Nav.Item>
-                      <Nav.Link eventKey="first">Tab 1</Nav.Link>
-                    </Nav.Item>
-                    <Nav.Item>
-                      <Nav.Link eventKey="second">Tab 2</Nav.Link>
-                    </Nav.Item>
-                    <Nav.Item>
-                      <Nav.Link eventKey="third">Tab 3</Nav.Link>
-                    </Nav.Item>
-                  </Nav>
-                  <Tab.Content id="slideInUp" className={isVisible ? "animate__animated animate__slideInUp" : ""}>
-                    <Tab.Pane eventKey="first">
-                      <Row>
-                        {
-                          projects.map((project, index) => {
-                            return (
-                              <ProjectCard
-                                key={index}
-                                {...project}
-                                />
-                            )
-                          })
-                        }
-                      </Row>
-                    </Tab.Pane>
-                    <Tab.Pane eventKey="section">
-                      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque quam, quod neque provident velit, rem explicabo excepturi id illo molestiae blanditiis, eligendi dicta officiis asperiores delectus quasi inventore debitis quo.</p>
-                    </Tab.Pane>
-                    <Tab.Pane eventKey="third">
-                      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque quam, quod neque provident velit, rem explicabo excepturi id illo molestiae blanditiis, eligendi dicta officiis asperiores delectus quasi inventore debitis quo.</p>
-                    </Tab.Pane>
-                  </Tab.Content>
-                </Tab.Container>
-              </div>}
+              {({ isVisible }) => (
+                <div
+                  className={
+                    isVisible ? "animate__animated animate__fadeIn" : ""
+                  }
+                >
+                  <h2>Projects</h2>
+                  <br></br>
+                  <Tab.Container id="projects-tabs" defaultActiveKey="first">
+                    <Nav
+                      variant="pills"
+                      className="nav-pills mb-5 justify-content-center align-items-center"
+                      id="pills-tab"
+                    >
+                      <Nav.Item>
+                        <Nav.Link eventKey="first">Tab 1</Nav.Link>
+                      </Nav.Item>
+                      <Nav.Item>
+                        <Nav.Link eventKey="second">Tab 2</Nav.Link>
+                      </Nav.Item>
+                    </Nav>
+
+                    <Tab.Content
+                      id="slideInUp"
+                      className={
+                        isVisible ? "animate__animated animate__slideInUp" : ""
+                      }
+                    >
+                      {/* TAB 1 */}
+                      <Tab.Pane eventKey="first">
+                        <div className="experiencia-laboral">
+                          <h3>Projects</h3>
+                          <p>
+                            Aquí describo brevemente mis roles y logros en cada
+                            empresa, además de las tecnologías y
+                            responsabilidades que tuve a mi cargo.
+                          </p>
+                          <Row>
+                            {projects.map((project, index) => {
+                              return <ProjectCard key={index} {...project} />;
+                            })}
+                          </Row>
+                        </div>
+                      </Tab.Pane>
+
+                      {/* TAB 2: Experiencia Laboral */}
+                      <Tab.Pane eventKey="second">
+                        <div className="experiencia-laboral">
+                          <h3>Experiencia Laboral</h3>
+                          <p>
+                            Aquí describo brevemente mis roles y logros en cada
+                            empresa, además de las tecnologías y
+                            responsabilidades que tuve a mi cargo.
+                          </p>
+                          <Row className="justify-content-center">
+                            <Col md={6}>
+                              <div className="exp-item">
+                                {/* Imagen circular en la esquina sup. derecha */}
+                                <div className="exp-image">
+                                  <img src={projImg3.src} alt="Imagen" />
+                                </div>
+                                
+                                <h4>Desarrollador Frontend</h4>
+                                <h5>Empresa XYZ | 2021 - Presente</h5>
+                                <p>
+                                  - Creación de interfaces responsivas con React
+                                  y Bootstrap <br />
+                                  - Mantenimiento y optimización de rendimiento{" "}
+                                  <br />- Colaboración con equipos de diseño y
+                                  backend
+                                </p>
+                              </div>
+
+                              <div className="exp-item">
+                                <h4>Desarrollador Full Stack</h4>
+                                <h5>Startup ABC | 2019 - 2021</h5>
+                                <p>
+                                  - Desarrollo y consumo de APIs REST <br />
+                                  - Implementación de bases de datos MySQL{" "}
+                                  <br />- Despliegue de aplicaciones en entornos
+                                  de producción
+                                </p>
+                              </div>
+
+                              <div className="exp-item">
+                                <h4>Intern de Desarrollo</h4>
+                                <h5>Compañía Demo | 2018 - 2019</h5>
+                                <p>
+                                  - Soporte en migraciones de proyectos <br />
+                                  - Documentación y testing <br />- Aprendizaje
+                                  de buenas prácticas en equipo ágil
+                                </p>
+                              </div>
+                            </Col>
+                          </Row>
+                        </div>
+                      </Tab.Pane>
+                    </Tab.Content>
+                  </Tab.Container>
+                </div>
+              )}
             </TrackVisibility>
           </Col>
         </Row>
       </Container>
-      <img className="background-image-right" src={colorSharp2.src}></img>
+      <img className="background-image-right" src={colorSharp2.src} />
     </section>
-  )
-}
+  );
+};
--- src/components/ProjectCard.jsx
+++ src/components/ProjectCard.jsx
@@ -3,13 +3,18 @@ import { Col } from "react-bootstrap";
 export const ProjectCard = ({ title, description, imgUrl }) => {
   return (
     <Col size={12} sm={6} md={4}>
-      <div className="proj-imgbx">
-        <img src={imgUrl} />
+      {/* Usamos un botón enfocable */}
+      <button
+        className="proj-imgbx"
+        tabIndex="0"
+        type="button" // Evita comportamientos extra si está dentro de <form>
+      >
+        <img src={imgUrl} alt={title} />
         <div className="proj-txtx">
           <h4>{title}</h4>
           <span>{description}</span>
         </div>
-      </div>
+      </button>
     </Col>
-  )
-}
+  );
+};
