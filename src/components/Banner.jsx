1de48ba8bb01b425003c3ff8bb1289d3eaba86f0
ALfredo <alfredobch2004@gmail.com>
2025-02-27 11:13:12 -0600

    Cambio de logos 1

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -54,7 +54,7 @@ export const Banner = () => {
                         <span className="tagline">Welcome to my Portfolio</span>
                         <h1>{"Hi I'm Berrocal "}<span className="wrap">{text}</span></h1>
                         <p>I'm a passionate Software Engineer from Mexico</p>
-                        <button onClick={() => console.log('connect')}>Let's connect <i className='bi bi-arrow-right-circle' size={25} /></button>
+                        <button onClick={() => document.getElementById('connect')?.scrollIntoView({ behavior: 'smooth' })}>Let's connect <i className='bi bi-arrow-right-circle' size={25} /></button>
                     </Col>
                     <Col xs={12} md={6} xl={5}>
                         <img src={headerImg.src} alt="Header Img" />
--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -1,6 +1,6 @@
-import meter1 from "../assets/img/meter1.svg";
-import meter2 from "../assets/img/meter2.svg";
-import meter3 from "../assets/img/meter3.svg";
+import linux from "../assets/img/linux1.png";
+import python from "../assets/img/python3.png";
+import git from "../assets/img/linux1.png";
 import Carousel from 'react-multi-carousel';
 import 'react-multi-carousel/lib/styles.css';
 import colorSharp from "../assets/img/color-sharp.png"
@@ -33,23 +33,39 @@ export const Skills = () => {
                 <div className="col-12">
                     <div className="skill-bx wow zoomIn">
                         <h2>Skills</h2>
-                        <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry.<br></br> Lorem Ipsum has been the industry's standard dummy text.</p>
+                        <p>These are the technologies and tools I have learned throughout my journey as a developer.</p>
                         <Carousel responsive={responsive} infinite={true} className="owl-carousel owl-theme skill-slider">
                             <div className="item">
-                                <img src={meter1.src} alt="Image" />
-                                <h5>Web Development</h5>
+                                <img src={linux.src} alt="Image" />
+                                <h5>Linux</h5>
                             </div>
                             <div className="item">
-                                <img src={meter2.src} alt="Image" />
-                                <h5>Brand Identity</h5>
+                                <img src={python.src} alt="Image" />
+                                <h5>Python</h5>
                             </div>
                             <div className="item">
-                                <img src={meter3.src} alt="Image" />
-                                <h5>Logo Design</h5>
+                                <img src={git.src} alt="Image" />
+                                <h5>Git</h5>
                             </div>
                             <div className="item">
-                                <img src={meter1.src} alt="Image" />
-                                <h5>Web Development</h5>
+                                <img src={linux.src} alt="Image" />
+                                <h5>Docker</h5>
+                            </div>
+                            <div className="item">
+                                <img src={linux.src} alt="Image" />
+                                <h5>PHP</h5>
+                            </div>
+                            <div className="item">
+                                <img src={linux.src} alt="Image" />
+                                <h5>MySQL</h5>
+                            </div>
+                            <div className="item">
+                                <img src={linux.src} alt="Image" />
+                                <h5>Laravel</h5>
+                            </div>
+                            <div className="item">
+                                <img src={linux.src} alt="Image" />
+                                <h5>React</h5>
                             </div>
                         </Carousel>
                     </div>
